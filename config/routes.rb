421c7bc71eb511235febf80fabf9db271315b21e
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-02 12:08:05 +0300

    added the mainpages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'main_pages/home'
+
+  get 'main_pages/help'
+
   resources :properties
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
-  root 'users#index'
+  root 'application#hello'
+  # root 'users#index'
 end
