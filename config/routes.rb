65c46896007a48261a5b5380a0d1623ed1834b82
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-03 13:57:40 +0300

    added tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'main_pages/home'
 
-  get 'main_pages/help'
 
   resources :properties
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'application#hello'
+  root 'users#index'
+
+  get  'main_pages/home'
+  get  'main_pages/help'
+  get  'main_pages/about'
   root 'application#hello'
-  # root 'users#index'
 end
