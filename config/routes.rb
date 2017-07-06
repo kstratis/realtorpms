f591394e3f2a45e489f7c727513270e75a7e82f2
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-06 16:22:24 +0300

    Finished the first part of the layourt

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
 
+  get 'users/new'
+
   # resources :properties
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
   # root 'users#index'
   root 'main_pages#home'
-  get  'main_pages/home'
-  get  'main_pages/help'
-  get  'main_pages/about'
-  get  'main_pages/contact'
+  get  '/help', to: 'main_pages#help'
+  get  '/about', to: 'main_pages#about'
+  get  '/contact', to: 'main_pages#contact'
+  get  '/signup',  to: 'users#new'
   # root 'application#hello'
 end
