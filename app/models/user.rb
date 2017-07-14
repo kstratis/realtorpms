08c2966db7e6782f91d24bc36f1cc17b7504bd65
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-14 19:44:28 +0300

    doing format validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,4 +24,19 @@ class UserTest < ActiveSupport::TestCase
     @user.email = '     '
     assert_not @user.valid?
   end
+
+  test 'first_name should not be too long' do
+    @user.first_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'last_name should not be too long' do
+    @user.last_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
 end
