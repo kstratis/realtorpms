41725d8f28dfc976d26b52d0e94e01ed0e458681
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-20 17:33:43 +0300

    fixed the signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   layout 'registration/main', except: [:show]  # show the barebones version only when signing up
-  def new
+
+  def new  # This is basically the registration page in GET
     @user = User.new
   end
 
   def show
     @user = User.find(params[:id]) # Automatically converts parameters from string to integer
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,13 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.first_name + user.last_name, class: 'gravatar')
   end
+
+  def inline_error(attribute, formatted_attribute)
+    if @user.errors[attribute].present?
+      render :partial => 'shared/error_inline_message',
+             :locals => {:error => @user.errors[attribute].first,
+                         :formatted_attribute => formatted_attribute}
+    end
+  end
+
 end
