975b8b267a1adf6b37eacd87a9d53c41c5993614
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-21 17:04:00 +0300

    getting there

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get 'sessions/new'
+  # get 'sessions/new'
 
   # get 'users/new'
 
@@ -11,10 +11,16 @@ Rails.application.routes.draw do
   # root 'application#hello'
   # root 'users#index'
   root 'main_pages#home'
+
   get  '/help', to: 'main_pages#help'
   get  '/about', to: 'main_pages#about'
   get  '/contact', to: 'main_pages#contact'
   get  '/signup',  to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
 
   # root 'application#hello'
