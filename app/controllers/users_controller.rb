7ae486d26167176aaaa16ce83048078c2196a4ef
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-21 12:47:14 +0300

    fixed bootstrap navbar toggle js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = 'Welcome to PropertyX'
+      redirect_to @user
       # Handle a successful save.
     else
+      # this merely re-renders the new template.
+      # It doesn't fully redirect (in other words it doesn't go through the +new+ method)
       render 'new'
-
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718121643) do
+ActiveRecord::Schema.define(version: 20170720145127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170718121643) do
     t.boolean "furnitures"
     t.boolean "fit_for_students"
     t.boolean "fireplace"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
