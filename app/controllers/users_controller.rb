19fa2ba0fbc18a7f8a6f668a41ff5d8cb515281a
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-07-26 19:36:21 +0300

    getting into friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   layout 'registration/main', except: [:show, :edit, :update]  # show the barebones version only when signing up
 
   # GET the new user registration page
@@ -45,5 +47,19 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  # Confirms a logged-in user.
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
