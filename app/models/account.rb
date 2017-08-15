e6c3042185ec1958aaf2c488a1205ea92d7e3402
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-08-15 18:30:20 +0300

    fixed the test

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,5 @@
 class Account < ApplicationRecord
-  validates :subdomain, presence: true, length: { maximum: 50 }
-  belongs_to :owner, class_name: 'User'
+  validates :subdomain, presence: true, length: { maximum: 50 }, uniqueness: true
+  belongs_to :owner, class_name: 'User', optional: true
   accepts_nested_attributes_for :owner
-
 end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -18,6 +18,23 @@ feature 'Accounts' do
     expect(page).to have_link('Signed is as demouser@example.com')
     expect(page.current_url).to eq('http://demoportal.lvh.me/')
   end
+
+  scenario 'Ensure subdomain uniqueness' do
+    Account.create!(subdomain: 'test')
+    visit new_account_path
+    fill_in 'Subdomain', with: 'test'
+    fill_in 'First name', with: 'demouser'
+    fill_in 'Last name', with: 'demouser_last_name'
+    fill_in 'Email', with: 'demouser@example.com'
+    fill_in 'Password', with: 'abc123abc' , exact: true
+    fill_in 'Password confirmation', with: 'abc123abc'
+    click_button 'Create account'
+    expect(page.current_url).to eq('http://lvh.me/accounts')  # This caters for turbolinks bug https://github.com/turbolinks/turbolinks/issues/251
+    expect(page).to have_content('Sorry, your account could not be created.') # General form error
+    subdomain_error = find('.pr-inline-form-error').text # Specific inline form error
+    expect(subdomain_error).to eq('Subdomain has already been taken.')
+  end
+
 end
 
 
