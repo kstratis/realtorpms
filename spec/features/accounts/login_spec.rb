c52484a8e2ce9988f2852f83cf59fd72d1b66136
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-08-25 11:42:26 +0300

    Fixed a couple of tests

--- spec/features/accounts/login_spec.rb
+++ spec/features/accounts/login_spec.rb
@@ -117,45 +117,49 @@ feature 'Logging in the system' do
   end
 
   context 'without subdomain just remember path' do
-    it 'can log in' do
-      visit 'http://lvh.me/users'
-      fill_in 'Email', with: account.owner.email
-      fill_in 'Password', with: account.owner.password
-      click_button 'Log in'
-      expect(page.current_url).to eq('http://test1.lvh.me/users')
+    it 'of /users will cause routing error because all routes are contraint protected' do
+      expect{ visit 'http://lvh.me/users' }.to raise_error(ActionController::RoutingError)
     end
 
-    it 'should not login' do
-      visit 'http://lvh.me/'
-      fill_in 'Email', with: account.owner.email
-      fill_in 'Password', with: account.owner.password
-      click_button 'Log in'
-      expect(page.current_url).not_to eq('http://test1.lvh.me/users')
+    it 'of /properties will cause routing error because all routes are contraint protected' do
+      expect{ visit 'http://lvh.me/properties' }.to raise_error(ActionController::RoutingError)
     end
 
-    it 'cannot log in with wrong password' do
-      visit 'http://lvh.me/users'
-      fill_in 'Email', with: account.owner.email
-      fill_in 'Password', with: 'password'
-      click_button 'Log in'
-      expect(page.current_url).to eq('http://lvh.me/login')
+    it 'of /properties will cause routing error because all routes are contraint protected' do
+      expect{ visit 'http://lvh.me/properties/new' }.to raise_error(ActionController::RoutingError)
     end
 
-    it 'cannot log in with wrong email' do
-      visit 'http://lvh.me/users'
-      fill_in 'Email', with: 'myemail@gmail.com'
-      fill_in 'Password', with: account.owner.password
-      click_button 'Log in'
-      expect(page.current_url).to eq('http://lvh.me/login')
-    end
-
-    it 'cannot log in with wrong username and password' do
-      visit 'http://lvh.me/users'
-      fill_in 'Email', with: 'myemail@gmail.com'
-      fill_in 'Password', with: 'mypassword'
-      click_button 'Log in'
-      expect(page.current_url).to eq('http://lvh.me/login')
-    end
+    # it 'should not login' do
+    #   visit 'http://lvh.me/'
+    #   fill_in 'Email', with: account.owner.email
+    #   fill_in 'Password', with: account.owner.password
+    #   click_button 'Log in'
+    #   expect(page.current_url).not_to eq('http://test1.lvh.me/users')
+    # end
+    #
+    # it 'cannot log in with wrong password' do
+    #   visit 'http://lvh.me/users'
+    #   fill_in 'Email', with: account.owner.email
+    #   fill_in 'Password', with: 'password'
+    #   click_button 'Log in'
+    #   expect(page.current_url).to eq('http://lvh.me/login')
+    # end
+    #
+    # it 'cannot log in with wrong email' do
+    #   visit 'http://lvh.me/users'
+    #   fill_in 'Email', with: 'myemail@gmail.com'
+    #   fill_in 'Password', with: account.owner.password
+    #   click_button 'Log in'
+    #   expect(page.current_url).to eq('http://lvh.me/login')
+    # end
+    #
+    # it 'cannot log in with wrong username and password' do
+    #   visit 'http://lvh.me/users'
+    #   fill_in 'Email', with: 'myemail@gmail.com'
+    #   fill_in 'Password', with: 'mypassword'
+    #   click_button 'Log in'
+    #   expect(page.current_url).to eq('http://lvh.me/login')
+    # end
 
   end
 
