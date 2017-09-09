54db43f77fcb20efd6e2a231475a53a357241674
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-09-09 17:40:51 +0300

    about to make breaking model changes part2

---
+++ spec/features/accounts/multi_account_user_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+describe 'Logging in the system without subdomain in url' do
+  let(:account) { FactoryGirl.create(:account, subdomain: 'test1') }
+  let(:account2) { FactoryGirl.create(:account2, subdomain: 'test2') }
+  let(:account3) { FactoryGirl.create(:account3, subdomain: 'test3') }
+  let(:userExternal) { FactoryGirl.create(:userExternal) }
+
+  context 'as a user' do
+
+    it 'who only owns an account' do
+      visit 'http://lvh.me/login'
+      fill_in 'Email', with: account.owner.email
+      fill_in 'Password', with: account.owner.password
+      click_button 'Log in'
+      expect(page.current_url).to eq('http://test1.lvh.me/')
+      expect(page).to have_content('This is the dashboard for logged in users')
+    end
+
+    it 'who owns an account but also is a member of another one' do
+      account2.users << account.owner
+      visit 'http://lvh.me/login'
+      fill_in 'Email', with: account.owner.email
+      fill_in 'Password', with: account.owner.password
+      click_button 'Log in'
+      expect(page.current_url).to eq('http://lvh.me/accounts')
+      expect(page).to have_content('Account switch screen.')
+    end
+
+    it 'who is just a member of an account' do
+      account2.users << userExternal
+      visit 'http://lvh.me/login'
+      fill_in 'Email', with: userExternal.email
+      fill_in 'Password', with: 'abc123'
+      click_button 'Log in'
+      expect(page.current_url).to eq('http://test2.lvh.me/')
+      expect(page).to have_content('This is the dashboard for logged in users')
+    end
+
+    it 'who is just a member of two or more accounts' do
+      account.users << userExternal
+      account2.users << userExternal
+      visit 'http://lvh.me/login'
+      fill_in 'Email', with: userExternal.email
+      fill_in 'Password', with: 'abc123'
+      click_button 'Log in'
+      expect(page.current_url).to eq('http://lvh.me/accounts')
+      expect(page).to have_content('Account switch screen.')
+    end
+
+
+
+
+  end
+end
