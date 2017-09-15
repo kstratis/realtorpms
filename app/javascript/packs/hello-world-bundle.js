78f39f50f76e2a20ed34ce5b7277b208eb5e7cb7
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2017-09-15 15:29:46 +0300

    new react files

---
+++ app/javascript/packs/hello-world-bundle.js
@@ -0,0 +1,10 @@
+import ReactOnRails from 'react-on-rails';
+
+import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
+import UsersList from '../bundles/HelloWorld/components/UsersList';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  HelloWorld,
+    UsersList
+});
