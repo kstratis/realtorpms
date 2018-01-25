f4f774d5040bab0083d76c89f7c06601e4803c69
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2018-01-25 16:32:06 +0200

    added card controls

--- app/javascript/bundles/datatables/components/PropertiesList.jsx
+++ app/javascript/bundles/datatables/components/PropertiesList.jsx
@@ -2,10 +2,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ReactPaginate from 'react-paginate';
 import withDatatable from './withDatatable';
+import SkyLight from 'react-skylight';
 
 const PropertiesList = ({handlePageClick, handleSort, handleAssign, advanceByTwo, isLoading, dataset, pageCount, selectedPage, sorting, ordering}) => {
   return (
+
     <div className="dataTablePage col-md-12">
+      <SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} title="Hi, I'm a simple modal">
+        Hello, I dont have any callback.
+      </SkyLight>
+      {/* CARD START */}
       {isLoading
         ? <div className={'centered'}>
             <div className={'spinner'}/>
@@ -17,8 +23,17 @@ const PropertiesList = ({handlePageClick, handleSort, handleAssign, advanceByTwo
                 <div className={'col-lg-3 col-md-4 col-xs-6 property-container'} key={entry.id}>
                   <div className={'property'}>
                     <div><a href={entry['view_entity_path']}>{entry['description']}</a></div>
-                  <div>{entry['size']}</div>
-                  <div>{entry['price']}</div>
+                    <div>{entry['size']}</div>
+                    <div>{entry['price']}</div>
+                    <div>{'hello world'}</div>
+                    <div className={'col-md-12 col-sm-12 col-xs-12 card-controls-container'}>
+                      <div className="btn-group btn-group-sm card-controls" role="group" aria-label="...">
+                        <button onClick={() => this.simpleDialog.show()}>Open Modal</button>
+                        <a className={'card-control-button btn btn-default'} role={'group'}><span><i className={'fa fa-eye fa-lg fa-fw '}></i></span></a>
+                        <a className={'card-control-button btn btn-default'} role={'group'}><span><i className={'fa fa-pencil fa-lg fa-fw'}></i></span></a>
+                        <a className={'card-control-button btn btn-default'} role={'group'}><span><i className={'fa fa-trash fa-lg fa-fw'}></i></span></a>
+                      </div>
+                    </div>
                   </div>
 
 
@@ -26,7 +41,9 @@ const PropertiesList = ({handlePageClick, handleSort, handleAssign, advanceByTwo
                 </div>
               ))}
             </div>
+             {/* CARD END */}
             <div className={'clearfix'}> </div>
+
             <ReactPaginate previousLabel={"❮"}
                            nextLabel={"❯"}
                            breakLabel={
