b8981854514ef8f1f46826c378db782dd3c009c0
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2018-03-22 17:04:46 +0200

    minor fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,19 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
+  # The original is this:
+  # config.assets.compile = false
+
+  # ******************************************
+  # The following lines were added
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.compress = true
+  config.assets.digest = true
+  config.assets.precompile += %w(.svg)
+  # ******************************************
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,11 @@ case Rails.env
   when 'development', 'test'
     options.merge!(domain: 'lvh.me')
   when 'production'
-    # TBA
+    # There was no value here
+    # I added lvh.me so that it runs on my local machine
+    # Should be changed however in production
+    options.merge!(domain: 'lvh.me')
+  # TBA
 end
 
 Propertyx::Application.config.session_store :cookie_store, options
