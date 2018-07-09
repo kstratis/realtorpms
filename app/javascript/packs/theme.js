4b30e653bdeacbb6fdf86ab32f77720f3ebc9e49
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2018-07-09 14:55:12 +0300

    more fixes

--- app/javascript/packs/theme.js
+++ app/javascript/packs/theme.js
@@ -1,14 +1,5 @@
 import App from "../bundles/themes/main_theme";
 
-// import StackedMenu from "stacked-menu/src/scripts/stacked-menu"
-// import "stacked-menu/src/scripts/stacked-menu.jquery"
-// import '!sass-loader!stacked-menu/src/scss/stacked-menu';
 $(document).on('turbolinks:load', function(e) {
   App.init();
 });
-
-// $(document).ready(function() {
-//   console.log( "ready!" );
-// });
-
-// App.init();
