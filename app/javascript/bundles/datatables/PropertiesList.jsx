2f81526720fd77bac196a99048f95a873ab64d14
Konstantinos Stratis <konos5@gmail.com>
2018-11-02 12:59:58 +0200

    many fixes and improvements

--- app/javascript/bundles/datatables/PropertiesList.jsx
+++ app/javascript/bundles/datatables/PropertiesList.jsx
@@ -4,68 +4,78 @@ import ReactPaginate from 'react-paginate';
 import withDatatable from './withDatatable';
 import SkyLight from 'react-skylight';
 
-const PropertiesList = ({handlePageClick, handleSort, handleAssign, advanceByTwo, isLoading, dataset, pageCount, selectedPage, sorting, ordering}) => {
+const PropertiesList = ({
+  handlePageClick,
+  handleSort,
+  handleAssign,
+  advanceByTwo,
+  isLoading,
+  dataset,
+  pageCount,
+  selectedPage,
+  sorting,
+  ordering
+}) => {
   return (
-
-    <div className="dataTablePage col-md-12">
-      <SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} title="Hi, I'm a simple modal">
+    <div className="col-md-12">
+      <SkyLight hideOnOverlayClicked ref={ref => (this.simpleDialog = ref)} title="Hi, I'm a simple modal">
         Hello, I dont have any callbacks.
       </SkyLight>
       {/* CARD START */}
-      {isLoading
-        ? <div className={'centered'}>
-            <div className={'spinner'}/>
-          </div>
-        : dataset.length > 0
-          ? <div className={'PropertyListContainer'}>
-            <div>
-              {dataset.map((entry) => (
-                <div className={'col-lg-3 col-md-4 col-xs-6 property-container'} key={entry.id}>
-                  <div className={'property'}>
-                    <div><a href={entry['view_entity_path']}>{entry['title']}</a></div>
-                    <div>{entry['size']}</div>
-                    <div>{entry['price']}</div>
-                    <div>{entry['description']}</div>
-                    <div className={'col-md-12 col-sm-12 col-xs-12 card-controls-container'}>
-                      <div className="btn-group btn-group-sm card-controls" role="group" aria-label="...">
-                        <a className={'card-control-button btn btn-default'} onClick={() => this.simpleDialog.show()} role={'group'}><span><i className={'fa fa-eye fa-lg fa-fw '}></i></span></a>
-                        <a className={'card-control-button btn btn-default'} role={'group'}><span><i className={'fa fa-pencil fa-lg fa-fw'}></i></span></a>
-                        <a className={'card-control-button btn btn-default'} role={'group'}><span><i className={'fa fa-trash fa-lg fa-fw'}></i></span></a>
-                      </div>
-                    </div>
+      {isLoading ? (
+        <div className={'centered'}>
+          <div className={'spinner'} />
+        </div>
+      ) : dataset.length > 0 ? (
+        <div className={'PropertyListContainer'}>
+          <div className={'row'}>
+            {dataset.map(entry => (
+              <div className={'col-sm-3'}>
+                <div className="card card-container" key={entry.id}>
+                  <img className="card-img-top card-image" src="https://via.placeholder.com/250x200" alt="Card image cap" />
+                  <div className="card-body">
+                    <h5 className="card-title">{entry.title}</h5>
+                    <p className="card-text">{entry.description}</p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
                   </div>
-
-
-
                 </div>
-              ))}
-            </div>
-             {/* CARD END */}
-            <div className={'clearfix'}> </div>
-
-            <ReactPaginate previousLabel={"❮"}
-                           nextLabel={"❯"}
-                           breakLabel={
-                             <span className="break-button-content"
-                                   onClick={advanceByTwo}>...</span>}
-                           breakClassName={"break-button"}
-                           pageCount={pageCount}
-                           marginPagesDisplayed={2}
-                           pageRangeDisplayed={5}
-                           onPageChange={handlePageClick}
-                           containerClassName={"pagination"}
-                           subContainerClassName={"pages pagination"}
-                           activeClassName={"active"}
-                           forcePage={selectedPage}
-                           pageClassName={"page"}
-                           nextClassName={'next'}
-                           previousClassName={'previous'}/>
+              </div>
+            ))}
           </div>
-          : <div className={"no-users"}>
-            <i className="pr-icon lg no-results"> </i>
-            <h3>No properties available.</h3>
+          {/* CARD END */}
+          {/*<div className={'clearfix'}> </div>*/}
+          <div className={'row'}>
+            <ReactPaginate
+              previousLabel={'❮'}
+              nextLabel={'❯'}
+              breakLabel={
+                <span className="break-button-content" onClick={advanceByTwo}>
+                  ...
+                </span>
+              }
+              breakClassName={'break-button'}
+              pageCount={pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={handlePageClick}
+              containerClassName={'pagination'}
+              subContainerClassName={'pages pagination'}
+              activeClassName={'active'}
+              forcePage={selectedPage}
+              pageClassName={'page'}
+              nextClassName={'next'}
+              previousClassName={'previous'}
+            />
           </div>
-      }
+        </div>
+      ) : (
+        <div className={'no-users'}>
+          <i className="pr-icon lg no-results"> </i>
+          <h3>No properties available.</h3>
+        </div>
+      )}
     </div>
   );
 };
