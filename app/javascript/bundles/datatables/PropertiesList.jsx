7e0d8c4d3dea7cfb99d965faaa3eb62d3ab19663
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-01-23 13:03:14 +0200

    wip

--- app/javascript/bundles/datatables/PropertiesList.jsx
+++ app/javascript/bundles/datatables/PropertiesList.jsx
@@ -112,7 +112,7 @@ const PropertiesList = ({
                   {/*<img className="card-img-top thumb" src={"https://picsum.photos/1250/800?image=1"} alt="Card image cap" />*/}
                   <div className="card-body">
                     <div className={'row'}>
-                      <div className={'col-10'}>
+                      <div className={'col-12'}>
                         <h5 className="card-title clamp-2">
                           <Highlighter
                             highlightClassName="highlighted"
@@ -122,11 +122,11 @@ const PropertiesList = ({
                           />
                         </h5>
                       </div>
-                      <div className={'col-2'}>
-                        <div className={'favourites'}>
+                      {/*<div className={'col-2'}>*/}
+                        {/*<div className={'favourites'}>*/}
 
-                        </div>
-                      </div>
+                        {/*</div>*/}
+                      {/*</div>*/}
                     </div>
 
                     <div className={'row'}>
