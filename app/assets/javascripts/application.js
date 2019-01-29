a15e3d21c48a39a2a38bb4d09a61585f7a1381da
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-01-29 18:27:36 +0200

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,6 +28,18 @@
 //= require stacked-menu/dist/js/stacked-menu.jquery.js
 //= require_tree .
 
+
+// After an ajax action a given button may either rewind history by 1
+// or return to a generic screen of our selection
+jQuery.fn.returnOnClick = function(location) {
+  $(this).on('click', function(e) {
+    e.preventDefault();
+    history.length === 2 ? Turbolinks.visit(location, { action: 'advance' }) : history.go(-1);
+  });
+  return this;
+};
+
+
 $(document).on('turbolinks:load', function(e) {
   console.log('running sdfsdf');
 
