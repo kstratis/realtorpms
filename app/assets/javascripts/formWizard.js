b5b2af9a681a85e73e549e1d926e98289fa328b7
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-01-29 16:47:53 +0200

    wip

--- app/assets/javascripts/formWizard.js
+++ app/assets/javascripts/formWizard.js
@@ -1,9 +1,30 @@
-$(document).on('turbolinks:load', function(e) {
-  $('#smartwizard').smartWizard({
-    theme: 'arrows',
-    transitionEffect: 'fade',
-    useURLhash: false,
-    showStepURLhash: false
-  });
-});
-
+// $(document).on('turbolinks:load', function(e) {
+//   $('#smartwizard').smartWizard({
+//     theme: 'arrows',
+//     transitionEffect: 'fade',
+//     useURLhash: false,
+//     showStepURLhash: false,
+//     anchorSettings: {
+//       anchorClickable: true,
+//       enableAllAnchors: true
+//     }
+//   });
+//
+//   $('#smartwizard').on('leaveStep', function(e, anchorObject, stepNumber, stepDirection) {
+//     console.log('leaving');
+//     $('#new_property').validate();
+//     // var elmForm = $('#form-step-' + stepNumber);
+//     // stepDirection === 'forward' :- this condition allows to do the form validation
+//     // only on forward navigation, that makes easy navigation on backwards still do the validation when going next
+//     // if (stepDirection === 'forward' && elmForm) {
+//     //   elmForm.validator('validate');
+//     //   var elmErr = elmForm.children('.has-error');
+//     //   if (elmErr && elmErr.length > 0) {
+//     //     // Form validation failed
+//     //     return false;
+//     //   }
+//     // }
+//     // return true;
+//   });
+// });
+//
--- app/assets/javascripts/properties/property_form_validator.js
+++ app/assets/javascripts/properties/property_form_validator.js
@@ -1,11 +1,32 @@
 $(document).on('turbolinks:load', function(e) {
   if (window.location.pathname === '/properties/new' || window.location.pathname.match(/^\/properties\/\d+\/edit$/)) {
-    // DEBUG
-    // console.log('Validation plugin loaded');
+
+    // Handle the property form dependant fields
+    // -------------------------------------
+    var elements = $('.dependent_check');
+    var status;
+    elements.each(function() {
+      status = $(this).prop('checked');
+      $(this)
+        .siblings()
+        .find('input.dependent_input')
+        .prop('disabled', !status);
+    });
+    elements.change(function() {
+      $(this)
+        .siblings()
+        .find('input.dependent_input')
+        .prop('disabled', !this.checked);
+    });
+    // -------------------------------------
+
+    // Handle the property form validators (new, edit)
+    // -------------------------------------
     var jqvalidatorDomNode = $('#jqvalidator');
     var jqtranslations = jqvalidatorDomNode.data('i18n').jqvalidator;
     $.extend($.validator.messages, jqtranslations);
-    $('#new_property, [id^=edit_property_]').validate({
+    console.log('validation runs');
+    var $v = $('#new_property, [id^=edit_property_]').validate({
       rules: {
         'property[businesstype]': {
           required: true
@@ -33,5 +54,43 @@ $(document).on('turbolinks:load', function(e) {
         }
       }
     });
+    // -------------------------------------
+
+
+    // Handle the property form wizard
+    // -------------------------------------
+    var $smartwizard = $('#smartwizard');
+
+    $smartwizard.smartWizard({
+      theme: 'arrows',
+      transitionEffect: 'fade',
+      useURLhash: false,
+      showStepURLhash: false,
+      anchorSettings: {
+        anchorClickable: true,
+        enableAllAnchors: true
+      }
+    });
+
+    $smartwizard.on('leaveStep', function(e, anchorObject, stepNumber, stepDirection) {
+      console.log('leaving');
+      if (!$v.form()){
+        return false;
+      }
+      // var elmForm = $('#form-step-' + stepNumber);
+      // stepDirection === 'forward' :- this condition allows to do the form validation
+      // only on forward navigation, that makes easy navigation on backwards still do the validation when going next
+      // if (stepDirection === 'forward' && elmForm) {
+      //   elmForm.validator('validate');
+      //   var elmErr = elmForm.children('.has-error');
+      //   if (elmErr && elmErr.length > 0) {
+      //     // Form validation failed
+      //     return false;
+      //   }
+      // }
+      // return true;
+    });
+    // -------------------------------------
+
   }
 });
