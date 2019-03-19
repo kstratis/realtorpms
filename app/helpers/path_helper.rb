bfe50b320746e9c5c6bc137ef56530f78e0d6199
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-03-19 16:05:44 +0200

    wip

---
+++ app/helpers/path_helper.rb
@@ -0,0 +1,10 @@
+module PathHelper
+  def path_printer(path)
+    root_path = '<li><a href="/"><i class="fas fa-home"> </i></a></li>'
+    partial_paths = ''
+    path.each do |entry|
+      partial_paths << entry == path.last ? "<li class='active'><span><%= t entry[:title] </span></li>" : "<li class='active'><span><%= t entry[:title] </span></li>"
+    end
+    root_path + partial_paths
+  end
+end
