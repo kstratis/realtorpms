3a3127a0ca458c16c64afd79f3f5e79c06fe2fc8
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-04-08 14:15:57 +0300

    more fixes and improvements in property add

--- app/javascript/packs/datepicker_localization.js
+++ app/javascript/packs/datepicker_localization.js
@@ -4,7 +4,10 @@ $(document).on('turbolinks:load', function(e) {
     if (active_locale !== 'en') {
       if (active_locale === 'el') {
         flatpickr.defaultConfig.locale = 'gr';
-        flatpickr.defaultConfig.dateFormat = "l, d M Y";
+        // flatpickr.defaultConfig.dateFormat = "l, d M Y";
+        flatpickr.defaultConfig.dateFormat = 'Z';
+        flatpickr.defaultConfig.altInput = true;
+        flatpickr.defaultConfig.altFormat = 'l, d M Y';
       } else {
         console.warn('Unknown lang selected for pickers.');
         // Need to load the lang and set it as
