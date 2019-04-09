7a24397f3f5a420b0b84c91af07ca113c6442f4c
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-04-09 12:32:45 +0300

    first part of reset password completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  # checks the email format
 
   self.per_page = 10 # This is for pagination
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :reset_token
   before_save { self.email = email.downcase }  # makes sure everything is lower case
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
@@ -121,6 +121,18 @@ class User < ApplicationRecord
   def unfavorite(property)
     favorites.where(property: property).destroy_all
     property.reload
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   def self.sorted_by_assignments_count(dataset)
