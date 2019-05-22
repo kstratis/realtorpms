43969268fd817693cbf678a08be5c515cfff9a2d
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-05-22 11:09:55 +0300

    optimizations 2

--- app/javascript/packs/components-bundle.js
+++ app/javascript/packs/components-bundle.js
@@ -1,20 +1,20 @@
-// import ReactOnRails from 'react-on-rails';
-// import UsersListWithDatatable from '../bundles/datatables/UsersList';
-// import UsersListAssignableWithDatatable from '../bundles/datatables/UsersListAssignable';
-// import PropertiesListWithDatatable from '../bundles/datatables/PropertiesList';
+import ReactOnRails from 'react-on-rails';
+import UsersListWithDatatable from '../bundles/datatables/UsersList';
+import UsersListAssignableWithDatatable from '../bundles/datatables/UsersListAssignable';
+import PropertiesListWithDatatable from '../bundles/datatables/PropertiesList';
 import SimpleSelect from '../bundles/components/SimpleSelect';
-// import DependantSelect from '../bundles/components/DependantSelect';
+import DependantSelect from '../bundles/components/DependantSelect';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 const registerComponents = () => {
-  // ReactOnRails.register({
+  ReactOnRails.register({
     // HelloWorld,
-    // UsersListWithDatatable,
-    // UsersListAssignableWithDatatable,
-    // PropertiesListWithDatatable,
-    // SimpleSelect,
-    // DependantSelect
-  // });
+    UsersListWithDatatable,
+    UsersListAssignableWithDatatable,
+    PropertiesListWithDatatable,
+    SimpleSelect,
+    DependantSelect
+  });
 };
 
 export default registerComponents;
