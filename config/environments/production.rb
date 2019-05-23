40bdfe20024c9b7e57c0d795f63af70941320348
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-05-23 19:33:04 +0300

    subdomain refactoring - hotfix 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_dispatch.tld_length = 3
+  # config.action_dispatch.tld_length = 2
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
