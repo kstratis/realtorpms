094142476f3389fac67842aa2bc0cb52ffc500cc
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2019-05-26 00:02:31 +0300

    wip

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -8,9 +8,8 @@ case Rails.env
   when 'staging'
     options.merge!(domain: 'dev.landia.io', tld_length: 2)
   when 'production'
-    # TBA
-    # options.merge!(domain: '.dev.landia.io', tld_length: 1)
+    options.merge!(domain: 'landia.io', tld_length: 1)
 end
 
 Propertyx::Application.config.session_store :cookie_store, options
-# Propertyx::Application.config.secret_key_base = 'abc123'
+Propertyx::Application.config.secret_key_base = 'abc123'
