ece8bb96121b0a2caee79af1428f7cde275b64cc
Konstantinos Stratis <konos5@gmail.com>
2019-07-11 00:29:58 +0300

    stable

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,6 +1,15 @@
 Grover.configure do |config|
   config.options = {
-      display_url: 'http://shakalaka.lvh.me:3000/'
+      display_url: 'http://shakalaka.lvh.me:3000/',
+      format: 'A4',
+      margin: {
+        top: '5px',
+        bottom: '10cm'
+      },
+      prefer_css_page_size: true,
+      emulate_media: 'screen',
+      cache: false,
+      timeout: 0 # Timeout in
       # cache: false
       # debug: {
       #   headless: false,  # Default true. When set to false, the Chromium browser will be displayed
