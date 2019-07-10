d25f69de6f841864aa38f6aec17550d41535640a
Konstantinos Stratis <konos5@gmail.com>
2019-07-10 16:42:41 +0300

    font fixes

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,7 +1,7 @@
 Grover.configure do |config|
   config.options = {
-      display_url: 'http://shakalaka.lvh.me:3000/',
-      cache: false
+      display_url: 'http://shakalaka.lvh.me:3000/'
+      # cache: false
       # debug: {
       #   headless: false,  # Default true. When set to false, the Chromium browser will be displayed
       #   devtools: true    # Default false. When set to true, the browser devtools will be displayed.
