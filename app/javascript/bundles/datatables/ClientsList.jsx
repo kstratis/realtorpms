b2db8def145977d5cf3c341675472c80134ec78b
Konstantinos Stratis <konos5@gmail.com>
2019-09-10 14:55:33 +0300

    clients wip 6 - format fixes

--- app/javascript/bundles/datatables/ClientsList.jsx
+++ app/javascript/bundles/datatables/ClientsList.jsx
@@ -7,25 +7,24 @@ import Spinner from './Spinner';
 import Avatar from '../components/Avatar';
 import { capitalizeFirstLetter } from '../utilities/helpers';
 
-
 const ClientsList = ({
-                     handlePageClick,
-                     handleSort,
-                     handleAssign,
-                     advanceByTwo,
-                     isLoading,
-                     dataset,
-                     pageCount,
-                     selectedPage,
-                     sorting,
-                     ordering,
-                     count,
-                     i18n,
-                     meta,
-                     handleSearchInput,
-                     handleFreezeUser,
-                     searchInput,
-                   }) => {
+  handlePageClick,
+  handleSort,
+  handleAssign,
+  advanceByTwo,
+  isLoading,
+  dataset,
+  pageCount,
+  selectedPage,
+  sorting,
+  ordering,
+  count,
+  i18n,
+  meta,
+  handleSearchInput,
+  handleFreezeUser,
+  searchInput
+}) => {
   return (
     <div className="users-list">
       <Spinner isLoading={isLoading} />
@@ -36,7 +35,9 @@ const ClientsList = ({
             <div className={'d-flex flex-row justify-content-end'}>
               <div className={'d-none d-lg-block pl-2'}>
                 <div className="search-count-container ">
-                  <span className="badge badge-pill badge-info p-2 mr-2">{`${capitalizeFirstLetter(i18n['result_count'])}: ${count}`}</span>
+                  <span className="badge badge-pill badge-info p-2 mr-2">{`${capitalizeFirstLetter(
+                    i18n['result_count']
+                  )}: ${count}`}</span>
                   {/*<strong className={'count'}>{count}</strong> <span>{i18n['result_count']}</span>*/}
                 </div>
               </div>
@@ -77,141 +78,143 @@ const ClientsList = ({
           <div className={'table-responsive'}>
             <table id="usersTable" className={`table table-striped ${isLoading ? 'reduced-opacity' : ''}`}>
               <thead>
-              <tr>
-                <th>
-                  <a
-                    id="sort_by_name"
-                    className={'sortable-header-name'}
-                    href={''}
-                    onClick={e => handleSort(e, 'last_name')}>
-                    <span>{i18n['datatable']['partner']}</span>
-                    {sorting === 'last_name' ? (
-                      ordering === 'asc' ? (
-                        <span className={'sortable-icon-container'}>
+                <tr>
+                  <th>
+                    <a
+                      id="sort_by_name"
+                      className={'sortable-header-name'}
+                      href={''}
+                      onClick={e => handleSort(e, 'last_name')}>
+                      <span>{i18n['datatable']['partner']}</span>
+                      {sorting === 'last_name' ? (
+                        ordering === 'asc' ? (
+                          <span className={'sortable-icon-container'}>
                             <i className={'fas fa-sort-up fa-fw'} />
                           </span>
-                      ) : (
-                        <span className={'sortable-icon-container'}>
+                        ) : (
+                          <span className={'sortable-icon-container'}>
                             <i className={'fas fa-sort-down fa-fw'} />
                           </span>
-                      )
-                    ) : (
-                      <i className={'fas fa-sort fa-fw'} />
-                    )}
-                  </a>
-                </th>
-                <th>
-                  <a
-                    id="sort_by_email"
-                    className={'sortable-header-name'}
-                    href={''}
-                    onClick={e => handleSort(e, 'email')}>
-                    <span>{i18n['datatable']['email']}</span>
-                    {sorting === 'email' ? (
-                      ordering === 'asc' ? (
-                        <span className={'sortable-icon-container'}>
+                        )
+                      ) : (
+                        <i className={'fas fa-sort fa-fw'} />
+                      )}
+                    </a>
+                  </th>
+                  <th>
+                    <a
+                      id="sort_by_email"
+                      className={'sortable-header-name'}
+                      href={''}
+                      onClick={e => handleSort(e, 'email')}>
+                      <span>{i18n['datatable']['email']}</span>
+                      {sorting === 'email' ? (
+                        ordering === 'asc' ? (
+                          <span className={'sortable-icon-container'}>
                             <i className={'fas fa-sort-up fa-fw'} />
                           </span>
-                      ) : (
-                        <span className={'sortable-icon-container'}>
+                        ) : (
+                          <span className={'sortable-icon-container'}>
                             <i className={'fas fa-sort-down fa-fw'} />
                           </span>
-                      )
-                    ) : (
-                      <i className={'fas fa-sort fa-fw'} />
-                    )}
-                  </a>
-                </th>
-                <th>
-                  <a
-                    id="sort_by_status"
-                    className={'sortable-header-name'}
-                    href={''}
-                    onClick={e => handleSort(e, 'status')}>
-                    <span>{i18n['datatable']['status']['title']}</span>
-                  </a>
-                </th>
-                <th>
-                  <a
-                    id="sort_by_date"
-                    className={'sortable-header-name'}
-                    href={''}
-                    onClick={e => handleSort(e, 'created_at')}>
-                    <span>{i18n['datatable']['registration']}</span>
-                    {sorting === 'created_at' ? (
-                      ordering === 'asc' ? (
-                        <span className={'sortable-icon-container'}>
+                        )
+                      ) : (
+                        <i className={'fas fa-sort fa-fw'} />
+                      )}
+                    </a>
+                  </th>
+                  <th>
+                    <a
+                      id="sort_by_status"
+                      className={'sortable-header-name'}
+                      href={''}
+                      onClick={e => handleSort(e, 'status')}>
+                      <span>{i18n['datatable']['status']['title']}</span>
+                    </a>
+                  </th>
+                  <th>
+                    <a
+                      id="sort_by_date"
+                      className={'sortable-header-name'}
+                      href={''}
+                      onClick={e => handleSort(e, 'created_at')}>
+                      <span>{i18n['datatable']['registration']}</span>
+                      {sorting === 'created_at' ? (
+                        ordering === 'asc' ? (
+                          <span className={'sortable-icon-container'}>
                             <i className={'fas fa-sort-up fa-fw'} />
                           </span>
-                      ) : (
-                        <span className={'sortable-icon-container'}>
+                        ) : (
+                          <span className={'sortable-icon-container'}>
                             <i className={'fas fa-sort-down fa-fw'} />
                           </span>
-                      )
-                    ) : (
-                      <i className={'fas fa-sort fa-fw'} />
-                    )}
-                  </a>
-                </th>
-                <th>
-                  <span>{i18n['datatable']['actions']}</span>
-                </th>
-              </tr>
+                        )
+                      ) : (
+                        <i className={'fas fa-sort fa-fw'} />
+                      )}
+                    </a>
+                  </th>
+                  <th>
+                    <span>{i18n['datatable']['actions']}</span>
+                  </th>
+                </tr>
               </thead>
               <tbody>
-              {dataset.map(entry => (
-                <tr className={'entry'} key={entry['id']}>
-                  <td className={'align-middle'}>
-                    <div className={'table-entry'}>
-                      <Avatar data={entry['avatar']} />
-                      <span>
+                {dataset.map(entry => (
+                  <tr className={'entry'} key={entry['id']}>
+                    <td className={'align-middle'}>
+                      <div className={'table-entry'}>
+                        <Avatar data={entry['avatar']} />
+                        <span>
                           <a className={'user-entry-color'} href={entry['view_entity_path']}>
                             {entry['name']}
                           </a>
                         </span>
-                    </div>
-                  </td>
-                  <td className={'align-middle'}>
-                    <div className={'table-entry'}>
-                      <span>{entry['email']}</span>
-                    </div>
-                  </td>
+                      </div>
+                    </td>
+                    <td className={'align-middle'}>
+                      <div className={'table-entry'}>
+                        <span>{entry['email']}</span>
+                      </div>
+                    </td>
 
-                  <td className={'align-middle'}>
-                    <div className={'table-entry'}>
-                      {entry['active']
-                        ? <span className="badge badge-success">{i18n['datatable']['status']['active']}</span>
-                        : <span className="badge badge-danger">{i18n['datatable']['status']['inactive']}</span> }
-                    </div>
-                  </td>
+                    <td className={'align-middle'}>
+                      <div className={'table-entry'}>
+                        {entry['active'] ? (
+                          <span className="badge badge-success">{i18n['datatable']['status']['active']}</span>
+                        ) : (
+                          <span className="badge badge-danger">{i18n['datatable']['status']['inactive']}</span>
+                        )}
+                      </div>
+                    </td>
 
-                  <td className={'align-middle'}>
-                    <div className={'table-entry'}>
-                      <span>{entry['registration']}</span>
-                    </div>
-                  </td>
+                    <td className={'align-middle'}>
+                      <div className={'table-entry'}>
+                        <span>{entry['registration']}</span>
+                      </div>
+                    </td>
 
-                  <td className={'align-middle action-btns'}>
-                    <a title={i18n['datatable']['tooltip_freeze_profile']}
-                      className="btn btn-md btn-icon btn-secondary btn-action "
-                      href={entry['edit_entity_path']}>
-                      <i className={`fas fa-pen`} />
-                    </a>
-                    <a
-                      title={i18n['datatable']['tooltip_delete_profile']}
-                      className={`btn btn-md btn-icon btn-secondary btn-action ${meta['is_admin'] ? '' : 'disabled'}`}
-                      href={entry['view_entity_path']}
-                      data-method="delete"
-                      data-confirm="Are you sure?"
-                      rel="nofollow">
-                      <i className="fas fa-trash user-delete" />
-                    </a>
-                  </td>
-                </tr>
-              ))}
+                    <td className={'align-middle action-btns'}>
+                      <a
+                        title={i18n['datatable']['tooltip_freeze_profile']}
+                        className="btn btn-md btn-icon btn-secondary btn-action "
+                        href={entry['edit_entity_path']}>
+                        <i className={`fas fa-pen`} />
+                      </a>
+                      <a
+                        title={i18n['datatable']['tooltip_delete_profile']}
+                        className={`btn btn-md btn-icon btn-secondary btn-action ${meta['is_admin'] ? '' : 'disabled'}`}
+                        href={entry['view_entity_path']}
+                        data-method="delete"
+                        data-confirm="Are you sure?"
+                        rel="nofollow">
+                        <i className="fas fa-trash user-delete" />
+                      </a>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
-
           </div>
           <div className={'clearfix'} />
           <div className={'d-none d-md-block'}>
