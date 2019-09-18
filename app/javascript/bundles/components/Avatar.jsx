3d7cf6a4134f3fa26878f65fed6e706c0be01581
Konstantinos Stratis <konos5@gmail.com>
2019-09-18 16:47:15 +0300

    refactoring user avatar - wip

--- app/javascript/bundles/components/Avatar.jsx
+++ app/javascript/bundles/components/Avatar.jsx
@@ -4,11 +4,18 @@ import { isUrl } from '../utilities/helpers';
 function Avatar(props) {
   return (
     <div className={`avatar-wrapper`}>
-      <div className={`${isUrl(props.data.url) ? 'user-avatar user-avatar-md mr-2' : ''}`}>
-      {isUrl(props.data.url)
-        ? <img className={`${props.data.classname}`} src={props.data.url} alt={'avatar'} />
-        : <div style={{backgroundColor: `${props.data.usercolor}`, color: '#ffffff'}} className="tile pr-tile tile-circle tile-md mr-2">{props.data.url}</div>
-      }
+      <div className={''}>
+        <div className={`${isUrl(props.data.url) ? 'user-avatar user-avatar-md mr-2' : ''}`}>
+          {isUrl(props.data.url) ? (
+            <img className={`${props.data.classname}`} src={props.data.url} alt={'avatar'} />
+          ) : (
+            <div
+              style={{ backgroundColor: `${props.data.usercolor}`, color: '#ffffff' }}
+              className="tile pr-tile tile-circle tile-md mr-2">
+              {props.data.url}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
