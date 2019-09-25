222a7c53e6301a55269cba7dc40ac05b3e9b61b0
Konstantinos Stratis <konos5@gmail.com>
2019-09-25 10:50:24 +0300

    fixes and improvements

--- app/controllers/accounts/dashboard_controller.rb
+++ app/controllers/accounts/dashboard_controller.rb
@@ -2,14 +2,14 @@ module Accounts
   class DashboardController < Accounts::BaseController
     def index
       @graph = {
-          labels: [I18n.t('main.chart.properties'), I18n.t('main.chart.users')],
+          labels: [I18n.t('main.chart.properties'), I18n.t('main.chart.users'), I18n.t('main.chart.clients')],
           datasets: [
               {
                   # label: I18n.t("main.chart.properties"),
                   backgroundColor: "rgba(163, 185, 210, 0.7)",
                   borderColor: "#4773a5",
                   lineTension: 0.000001,
-                  data: [current_account.properties.count, current_account.users.count]
+                  data: [current_account.properties.count, current_account.users.count, current_account.clients.count]
               }
           ]
       }
--- app/javascript/packs/theme.js
+++ app/javascript/packs/theme.js
@@ -4,8 +4,16 @@ import Chart from 'chart.js';
 function completionTasksChart (themeInstance) {
 
   const data = JSON.parse(document.getElementById('stats_data').dataset.graph);
-  data.datasets[0].backgroundColor = themeInstance.getColors('brand').indigo;
-  data.datasets[0].borderColor= themeInstance.getColors('brand').indigo;
+  // data.datasets[0].backgroundColor = themeInstance.getColors('brand').indigo;
+  data.datasets[0].backgroundColor = [
+    // themeInstance.getColors('brand').red,
+    themeInstance.getColors('brand').purple,
+    // themeInstance.getColors('brand').indigo,
+    themeInstance.getColors('brand').teal,
+    themeInstance.getColors('brand').yellow,
+  ];
+  // data.datasets[0].borderColor = themeInstance.getColors('brand').indigo;
+  data.datasets[0].borderColor = '#ffffff';
 //   const data = {
 //     labels: ['apples', 'tomatoes'],
 //     datasets: [
@@ -26,49 +34,62 @@ function completionTasksChart (themeInstance) {
 //         data: [150]
 //       }]
 // };
-    // datasets: [{
-    //   backgroundColor: themeInstance.getColors('brand').indigo,
-    //   borderColor: themeInstance.getColors('brand').indigo,
-    //   data: [155, 65, 465, 265, 225, 325, 80]
-    // }]
+  // datasets: [{
+  //   backgroundColor: themeInstance.getColors('brand').indigo,
+  //   borderColor: themeInstance.getColors('brand').indigo,
+  //   data: [155, 65, 465, 265, 225, 325, 80]
+  // }]
+
 
   // init chart bar
-  const canvas = $('canvas.chart')[0].getContext('2d');
-  let chart = new Chart(canvas, {
-    type: 'bar',
+  const canvas1 = $('canvas.chart')[0].getContext('2d');
+  let chart1 = new Chart(canvas1, {
+    type: 'doughnut',
     data: data,
     options: {
       responsive: true,
       maintainAspectRatio: false,
-      legend: { display: false },
+      legend: { display: true, position: 'bottom' },
       title: { display: false },
-      scales: {
-        xAxes: [{
-          gridLines: {
-            display: true,
-            drawBorder: false,
-            drawOnChartArea: false
-          },
-          ticks: {
-            maxRotation: 0,
-            maxTicksLimit: 3
-          }
-        }],
-        yAxes: [{
-          gridLines: {
-            display: true,
-            drawBorder: false
-          },
-          ticks: {
-            beginAtZero: true,
-            stepSize: 100
-          }
-        }]
-      }
+      // scales: {
+      //   xAxes: [{
+      //     gridLines: {
+      //       display: true,
+      //       drawBorder: false,
+      //       drawOnChartArea: false
+      //     },
+      //     ticks: {
+      //       maxRotation: 0,
+      //       maxTicksLimit: 3
+      //     }
+      //   }],
+      //   yAxes: [{
+      //     gridLines: {
+      //       display: true,
+      //       drawBorder: false
+      //     },
+      //     ticks: {
+      //       beginAtZero: true,
+      //       stepSize: 100
+      //     }
+      //   }]
+      // }
     }
-  })
-}
+  });
 
+
+  // const canvas2 = $('canvas.chart')[1].getContext('2d');
+  // let chart2 = new Chart(canvas2, {
+  //   type: 'doughnut',
+  //   data: data,
+  //   options: {
+  //     responsive: true,
+  //     maintainAspectRatio: false,
+  //     legend: { display: true },
+  //     title: { display: false },
+  //   }
+  // });
+}
 
 $(document).on('turbolinks:load', function(e) {
   // Initialize the perfect scrollbar plugin
