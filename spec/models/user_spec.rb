363006416c3ff8f58b34a8b12c7db71cac29b849
Konstantinos Stratis <konos5@gmail.com>
2019-09-27 10:51:42 +0300

    Deleting users fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe User, type: :model do
   end
 
   # We are not testing favlists or favorites
-   it "can successfully be deleted when is an account owner" do
+  it "can successfully be deleted when is an account owner" do
     @account1_user_count = @account1.users.count
     # regular users
     expect(@account1.users.count).to eq(@account1_user_count)
@@ -96,6 +96,66 @@ RSpec.describe User, type: :model do
     expect { @account1.owner.destroy }.to change { Account.count }.by(-1)
                                               .and change { User.count }.by(-1)
                                                        .and change { Membership.count }.by(-@account1_user_count) # owner are not subject to membeships join table
+  end
+
+
+  # This is an attempt to mimic the +destroy+ action of the users controller (multi account user)
+  it "can successfully be deleted when is a regular user and belongs to more than one account" do
+    # The first regular user of account 2 starts off belonging to 1 account
+    expect(@account2_first_user.get_account_count).to eq(1)
+
+    # He's got no properties assigned but we give me a couple and confirm.
+    expect(@account2_first_user.properties.count).to eq(0)
+    @account2_first_user.properties << @account2_properties.first
+    @account2_first_user.properties << @account2_properties.second
+    expect(@account2_first_user.properties.count).to eq(2)
+
+    # We then add him to Account 1
+    @account1.users << @account2_first_user
+    # and assign him 3 properties from account 1
+    @account2_first_user.properties << @account1_properties.first
+    @account2_first_user.properties << @account1_properties.second
+    @account2_first_user.properties << @account1_properties.third
+
+    # verify that @account2_first_user now totally has 5 properties. 2 from his original account and 3 from the new one
+    expect(@account2_first_user.properties.count).to eq(5)
+
+    # Delete the user from the new account. Mimic the controller action +destroy+
+    expect do
+      Membership.find_by(account: @account1, user: @account2_first_user).destroy # This one is unique
+      Assignment.where(user: @account2_first_user).joins(:property).where(properties: {account: @account1}).try(:destroy_all)
+      Clientship.where(user: @account2_first_user).joins(:client).where(clients: {account: @account1}).try(:destroy_all)
+      Favlist.where(account: @account1, user: @account2_first_user).try(:destroy_all)
+    end.to change { Membership.count }.by(-1)
+               .and change { Assignment.count }.by(-3)
+                        .and not_change { User.count }
+                                 .and not_change { Account.count }
+                                          .and not_change { Clientship.count }
+                                                   .and not_change { Favlist.count }
+
+  end
+
+  # This is an attempt to mimic the +destroy+ action of the users controller (single account user)
+  it "can successfully be deleted when is a regular user and belongs to a single account" do
+    # The first regular user of account 2 starts off belonging to 1 account
+    expect(@account2_first_user.get_account_count).to eq(1)
+
+    # He's got no properties assigned but we give me a couple and confirm.
+    expect(@account2_first_user.properties.count).to eq(0)
+    @account2_first_user.properties << @account2_properties.first
+    @account2_first_user.properties << @account2_properties.second
+    expect(@account2_first_user.properties.count).to eq(2)
+    expect(@account2.users.count).to eq(2)
+
+    # Delete the user from the new account. Mimic the controller action +destroy+
+    expect { @account2_first_user.destroy }.to change { Assignment.count }.by(-2)
+                                                   .and change { Membership.count }.by(-1)
+                                                            .and change { User.count }.by(-1)
+                                                                     .and not_change { Account.count }
+                                                                              .and not_change { Clientship.count }
+                                                                                       .and not_change { Favlist.count }
+                                                                                                .and not_change { Property.count }
+
   end
 
 end
