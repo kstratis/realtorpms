b970d12c653e1ef073a82a717dda199485fb683e
Konstantinos Stratis <konos5@gmail.com>
2019-10-08 00:55:45 +0300

    custom fields - stable 3

--- app/javascript/packs/custom_fields.js
+++ app/javascript/packs/custom_fields.js
@@ -12,17 +12,14 @@ $(document).on('turbolinks:load', function(e) {
   //   event.preventDefault();
   //   popupWindow($(this).data('url'), null, "900", "600");
   // });
-  // $(document).on('click', 'form .remove_fields', function(event) {
-  $('form .remove_fields').on('click', function(event) {
+  $(document).on('click', 'form .remove_fields', function(event) {
     console.log('remove running');
     $(this).prev('input[type=hidden]').val('1');
     $(this).closest('.form-row').hide();
     event.preventDefault();
   });
 
-  // $(document).on('click', 'form .add_fields', function(event) {
-  $('form .add_fields').on('click', function(event) {
-
+  $(document).on('click', 'form .add_fields', function(event) {
     console.log('add running');
     const time = new Date().getTime();
     const regexp = new RegExp($(this).data('id'), 'g');
