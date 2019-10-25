21c527236940f5baea65393bbcf2a0d30e5ceed9
Konstantinos Stratis <konos5@gmail.com>
2019-10-25 17:34:21 +0300

    cfields refactored 2

--- app/controllers/concerns/cfields.rb
+++ app/controllers/concerns/cfields.rb
@@ -10,8 +10,6 @@ module Cfields
     initial_cfields = Hash.new
     cfields_dump = get_cfields(name)
     cfields_raw = filters.keys.grep(/^cfield_/)
-    puts 'POPOPOPOPOPOPO'
-    puts entity.inspect
     if cfields_raw.any?
       cfields = cfields_raw.map { |cfield| cfield.to_s.split('_')[1..].join('_') }
       cfields.each do |cfield|
@@ -20,10 +18,8 @@ module Cfields
         cfield_type = cfields_dump.select { |cf| cf[cfield.to_sym] ? cf[cfield.to_sym] : nil }.first[cfield.to_sym].field_type
         case cfield_type
         when 'dropdown', 'check_box'
-          puts "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
           entity = entity.where('preferences @> ?', entry.to_json)
         when 'text_field'
-          puts "tttttttttttttttttttttttttttttttttttttttttttttttt"
           entity = entity.where("preferences ->> ? ilike '%#{filters["cfield_#{cfield}"]}%'", cfield)
         else
           'Error filter - contact support'
