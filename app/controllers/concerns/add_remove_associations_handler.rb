15960ae3977f5fce7ef24ab1c040e5df6d77fa72
Konstantinos Stratis <konos5@gmail.com>
2019-11-14 17:00:59 +0200

    wip unstable heavy

--- app/controllers/concerns/add_remove_associations_handler.rb
+++ app/controllers/concerns/add_remove_associations_handler.rb
@@ -3,7 +3,7 @@ module AddRemoveAssociationsHandler
 
   # Add remove accociation entities based on each request (dropdown selection)
   #
-  # @param object [Object] The object in question. i.e. property, user
+  # @param object [Object] The object in question. i.e. property or user
   # @param association [String] The association object. i.e For 'clients' that would be property.send('clients')
   # @param selection [Array] That's an array of objects.
   #   i.e. [{"label"=>"John Smith", "value"=>36}, {"label"=>"Jane Stevens", "value"=>"Jane Stevens", "__isNew__"=>true}],
@@ -12,15 +12,18 @@ module AddRemoveAssociationsHandler
     puts 'handler working'
     puts "object is: #{object.inspect}"
     # Fetch all existing associations
-    existing_associations = object.send(association).map(&:id)
+    existing_association_ids = object.send(association).pluck(:id)
 
     # ----
 
     # Normalize the data to an array
     requested_user_assignments = ru.map(&:to_h).map { |hash| hash['value'] }
 
+
+
+
     # The calculated user ids to be remove from the property
-    remove_ids = existing_associations - requested_user_assignments
+    remove_ids = existing_association_ids - requested_user_assignments
     # The calculated user ids to be added to the property
     add_ids = requested_user_assignments - existing_user_assignments
 
