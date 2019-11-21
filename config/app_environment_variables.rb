621b8d38599dd1074939c0360d231a79761ce30d
Konstantinos Stratis <konos5@gmail.com>
2019-11-21 16:03:05 +0200

    fixed a seed issue

--- config/app_environment_variables.rb
+++ config/app_environment_variables.rb
@@ -1,11 +1,11 @@
 if Rails.env.development?
-  ENV['SEEDS_MEDIA_DIR'] = '/Users/kstratis/efi_pics/'
+  ENV['MEDIA_DIR'] = '/Users/kstratis/efi_pics/'
 end
 
 if Rails.env.staging?
-  ENV['SEEDS_MEDIA_DIR'] = '/home/deploy/media/'
+  ENV['MEDIA_DIR'] = '/home/deploy/media/'
 end
 
 if Rails.env.production?
-  ENV['SEEDS_MEDIA_DIR'] = '/home/deploy/media/'
+  ENV['MEDIA_DIR'] = '/home/deploy/media/'
 end
