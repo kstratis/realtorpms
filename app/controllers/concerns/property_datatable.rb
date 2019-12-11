4ef482136e9759aef1f207edff651c78c3ee7b9f
Konstantinos Stratis <konos5@gmail.com>
2019-12-11 17:40:20 +0200

    properties index photo variant fix

--- app/controllers/concerns/property_datatable.rb
+++ app/controllers/concerns/property_datatable.rb
@@ -133,7 +133,7 @@ module PropertyDatatable
             edit_entity_path: edit_property_path(property),
             fav_entity_path: property_favorites_path(property),
             purpose: I18n.t("activerecord.attributes.property.enums.businesstype.#{property.businesstype}_banner"),
-            avatar: property.avatar.attached? ? url_for(property.avatar) : nil,
+            avatar: property.avatar.attached? ? url_for(property.avatar.variant(resize: "30%")) : nil,
             slug: property.slug,
             # isFaved: property.is_faved_by?(current_user),
             # assignments: property.properties.count,
