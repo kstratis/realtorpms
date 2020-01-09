9a2387d6fe448bdafbf982b68dfda36b22b1f54b
Konstantinos Stratis <konos5@gmail.com>
2020-01-09 19:13:38 +0200

    more fixes

--- app/controllers/accounts/assignments_controller.rb
+++ app/controllers/accounts/assignments_controller.rb
@@ -2,17 +2,20 @@ module Accounts
   class AssignmentsController < Accounts::BaseController
     include AddRemoveAssociationsHandler
 
-    before_action :authorize_owner_rest!
+    before_action :admin_access_ajax
 
     # Calculates the diff between the existing and the requested assignments of a given property and applies it.
     def assign
+      #render json: error_response, status: 403 and return unless current_user.role(current_account) == 'admin'
       # Get the requested property
       property = current_account.properties.find(assignment_params[:pid])
       data = associations_handler(property, 'users', assignment_params[:selection])
+
       render :json => {:status => 200, :message => data, meta: I18n.t('js.components.select.assign_completed')} and return
     end
 
     def assigned
+      #render json: error_response, status: 403 and return unless current_user.role(current_account) == 'admin'
       property = current_account.properties.find(assignment_params[:pid])
       data = property.users.blank? ? nil : property.users.map { |user| {label: "#{user.first_name} #{user.last_name}", value: user.id} }
       render json: {status: "OK", message: data}
@@ -25,13 +28,6 @@ module Accounts
         params.permit(:pid, selection: [:label, :value], assignment: {})
       end
 
-      def authorize_owner_rest!
-        unless owner?
-          respond_to do |format|
-            format.json {render json: {message: 'Forbidden'}, status: 403 }
-          end
-        end
-      end
   end
 end
 
--- app/controllers/concerns/roles_access.rb
+++ app/controllers/concerns/roles_access.rb
@@ -1,10 +1,16 @@
 module RolesAccess
   extend ActiveSupport::Concern
 
+  def admin_access_ajax
+    render json: {status: I18n.t('error'), message: I18n.t('action_not_permitted_error')},
+           status: 403 unless current_user.role(current_account) == 'admin'
+  end
+
   def admin_access
     unless current_user.is_admin?(current_account)
-      flash[:danger] = I18n.t('access_denied')
+      flash[:danger] = I18n.t('action_not_permitted_error')
       redirect_to root_path
     end
   end
+
 end
