48c009fb46d4eefb0252240080ff7394191e9bb9
Konstantinos Stratis <konos5@gmail.com>
2020-01-31 16:31:36 +0200

    another final fix 3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/(assets|packs)/*',
-             headers: :any,
-             methods: [:get]
+    resource '/assets/*'
+      # resource '/(assets|packs)/*',
+    # headers: :any,
+    # methods: [:get]
   end
 end
