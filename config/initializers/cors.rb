7d6c2359f656e5a9a4c41ec8ee5c1f2d32ab0c37
Konstantinos Stratis <konos5@gmail.com>
2020-01-31 11:45:23 +0200

    minor cdn fix 3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
-    resource '/assets/*',
+    resource '/(assets|packs)/*',
              headers: :any,
              methods: [:get]
   end
