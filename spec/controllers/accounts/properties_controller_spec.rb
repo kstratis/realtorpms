1fd10bf56095c5f4ef2cc6c82c052790d888f21e
Konstantinos Stratis <kstratis@skroutz.gr>
2020-08-11 00:34:53 +0300

    progressing with tests

---
+++ spec/controllers/accounts/properties_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe Accounts::PropertiesController, type: :controller do
+  let(:account) { FactoryBot.create(:account) }
+  let(:property) {FactoryBot.create(:property, account: account)}
+  let(:account_owner) { account.owner }
+  let(:partners) { FactoryBot.create_list(:usersequence, 4) }
+
+  before do
+    @request.host = "#{account.subdomain}.lvh.me"
+    log_in(account_owner)
+  end
+
+  describe 'GET #index' do
+    subject { get :index, params: {}, format: :json }
+
+    before do
+      account.users << partners.first
+      account.users << partners.second
+    end
+
+    it { is_expected.to be_successful }
+
+    it { is_expected.to have_http_status(200) }
+
+    it "contains the total_pages attribute" do
+      subject
+      expect(JSON.parse(response.body)).to have_key('results_per_page')
+    end
+
+    it "contains the total_count attribute" do
+      subject
+      expect(JSON.parse(response.body)).to have_key('datalist')
+    end
+
+    it "contains the total_per_page attribute" do
+      subject
+      expect(JSON.parse(response.body)).to have_key('total_entries')
+    end
+
+    it "contains the total_per_page attribute" do
+      subject
+      expect(JSON.parse(response.body)).to have_key('current_page')
+    end
+  end
+end
--- spec/support/factories/property_factory_static.rb
+++ spec/support/factories/property_factory_static.rb
@@ -1,5 +1,26 @@
 FactoryBot.define do
 
+  factory :property, :class => Property do
+    businesstype { [:sell, :rent, :sell_rent].sample }
+    description { Faker::Movies::Ghostbusters.quote }
+    category { Category.find((5..32).to_a.sample) }
+    location {Location.find(102723)}
+    size {rand(25..5000)}
+    price { rand(5000..5000000) }
+    bedrooms {rand(1..5)}
+    floor { rand(0..10) }
+    construction {rand(1970..2018) }
+
+    transient do
+      account { Account.first }
+    end
+
+    before(:create) do |property, evaluator|
+      property.account = evaluator.account
+    end
+
+  end
+
   factory :propertysequence, :class => Property do
     businesstype { [:sell, :rent, :sell_rent].sample }
     sequence(:description) { |n| "propertyNew#{n}" }
@@ -18,6 +39,7 @@ FactoryBot.define do
       property.account = evaluator.account
       property.category = evaluator.category
       property.location = evaluator.location
+      property.model_type = evaluator.account.model_types.find_by(name: 'properties')
       # property.category = evaluator.category
       # FactoryBot.find(:account, property: propertysequence)
     end
