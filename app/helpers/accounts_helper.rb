7b14eac73c4b3c304ec18605993867c75d106a48
Konstantinos Stratis <kstratis@skroutz.gr>
2021-01-08 19:10:24 +0200

    wip

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -6,4 +6,13 @@ module AccountsHelper
   # def display_flash_messages
   #   render 'shared/flash_messages', :messages => flash
   # end
+  #
+  def redirect_to_app_url
+    user_accounts = current_user.all_accounts
+    if user_accounts.present? && user_accounts.size == 1
+      root_url(subdomain: user_accounts.first.subdomain)
+    else
+      account_list_url(subdomain: nil)
+    end
+  end
 end
