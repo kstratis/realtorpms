7bd7a658a50f7e11f1104623127bef311bcbeadc
Konstantinos Stratis <kstratis@skroutz.gr>
2021-06-01 12:12:15 +0300

    revert last commit

--- app/services/cloudfront_denier.rb
+++ app/services/cloudfront_denier.rb
@@ -7,20 +7,18 @@ class CloudfrontDenier
   end
 
   def call(env)
-    # if cloudfront?(env) && !asset?(env)
-    #   [302, { 'Location' => @target }, []]
-    # else
-    #   @app.call(env)
-    # end
-    # update
-    @app.call(env)
+    if cloudfront?(env) && !asset?(env)
+      [302, { 'Location' => @target }, []]
+    else
+      @app.call(env)
+    end
   end
 
-  # def asset?(env)
-  #   env['PATH_INFO'] =~ %r{^/assets}
-  # end
-  #
-  # def cloudfront?(env)
-  #   env['HTTP_USER_AGENT'] == 'Amazon CloudFront'
-  # end
+  def asset?(env)
+    env['PATH_INFO'] =~ %r{^/assets}
+  end
+
+  def cloudfront?(env)
+    env['HTTP_USER_AGENT'] == 'Amazon CloudFront'
+  end
 end
