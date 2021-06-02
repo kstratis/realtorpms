975b89fbdd219ecd00361c0c199d73a1718dcfcb
Konstantinos Stratis <kstratis@skroutz.gr>
2021-06-02 08:33:43 +0300

    cors fixup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/assets/*'
+    resource '/assets/*',
       # resource '/(assets|packs)/*',
-    # headers: :any,
-    # methods: [:get]
+             headers: :any,
+             methods: [:get]
   end
 end
