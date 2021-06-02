a4f5767db56ef875c06ef007a206a7c8667f24fa
Konstantinos Stratis <kstratis@skroutz.gr>
2021-06-02 09:11:39 +0300

    webpacker fixup 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,17 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %w[
-      https://realtorpms.com
-      http://realtorpms.com
-      https://*.realtorpms.com
-      http://*.realtorpms.com
-    ]
-    resource '/assets/*'
+    # origins %w[
+    #   https://realtorpms.com
+    #   http://realtorpms.com
+    #   https://*.realtorpms.com
+    #   http://*.realtorpms.com
+    # ]
+    origins '*'
+    resource '/assets/*',
+             headers: :any,
+             methods: [:get, :post, :options]
+
       # resource '/(assets|packs)/*',
       #        headers: :any,
       #        methods: [:get]
