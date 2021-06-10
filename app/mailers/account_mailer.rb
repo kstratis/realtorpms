b2639dca41df45e521352ed31dd2ec2bab005b2f
Konstantinos Stratis <kstratis@skroutz.gr>
2021-06-10 12:47:32 +0300

    form fixes and improvements

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -6,15 +6,4 @@ class AccountMailer < ApplicationMailer
     mail to: @user.email, subject: "Account activation"
   end
 
-  # def password_reset(user, subdomain)
-  #   @user = user
-  #   @subdomain = subdomain
-  #   mail to: user.email, subject: I18n.t('sessions.passw_reset', brand: BRANDNAME.capitalize)
-  # end
-  #
-  # def welcome(user, subdomain)
-  #   @user = user
-  #   @subdomain = subdomain
-  #   mail to: user.email, subject: I18n.t('sessions.welcome', brand: BRANDNAME.capitalize)
-  # end
 end
