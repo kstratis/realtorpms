ecffc5f5fd341788108f4335e027e697776d94b6
Konstantinos Stratis <kstratis@skroutz.gr>
2021-10-08 23:47:59 +0300

    fixup! website pinning completed

--- app/javascript/bundles/datatables/PropertyEntry.jsx
+++ app/javascript/bundles/datatables/PropertyEntry.jsx
@@ -52,7 +52,6 @@ class PropertyEntry extends React.Component {
           </div>
 
           <div className="list-group list-group-media mb-3 d-none d-sm-block">
-
             <a
               href={this.props.entry['allow_view'] ? this.props.entry['view_entity_path'] : ''}
               className={`list-group-item list-group-item-action property-index-avatar ${
