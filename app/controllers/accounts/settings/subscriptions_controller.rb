bebc719e235e66d081e99805a672b62956fcc564
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2022-04-04 20:30:00 +0300

    yet another fix

--- app/controllers/accounts/settings/subscriptions_controller.rb
+++ app/controllers/accounts/settings/subscriptions_controller.rb
@@ -65,10 +65,7 @@ module Accounts
       end
 
       def retrieve_checkout_url
-        Rails.logger.info('Printing the image url')
         url = 'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link'
-        image_url = view_context.asset_pack_path('media/images/png_logo.png')
-        Rails.logger.info(image_url)
         request_body = {
           "vendor_id" => 5206,
           "vendor_auth_code" => "14d4ccf4f2d54b00a93a259a56cbd9aebe222060c888fc22fe",
@@ -77,7 +74,7 @@ module Accounts
           "customer_email" => current_user.email,
           "return_url" => settings_subscription_completed_path,
           "title" => "RealtorPMS subscription",
-          "image_url" => image_url
+          "image_url" => view_context.asset_pack_path('media/images/png_logo.png')
         }
         # 'vendor_id=5206&vendor_auth_code=14d4ccf4f2d54b00a93a259a56cbd9aebe222060c888fc22fe&product_id=&title=&webhook_url=&prices=&recurring_prices=&trial_days=&custom_message=&coupon_code=&discountable=&image_url=&return_url=&quantity_variable=&quantity=&expires=&marketing_consent=&customer_email=&customer_country=&customer_postcode=&is_recoverable=&passthrough=&vat_number=&vat_company_name=&vat_street=&vat_city=&vat_state=&vat_country=&vat_postcode='
 
