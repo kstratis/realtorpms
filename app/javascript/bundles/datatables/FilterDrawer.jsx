b91202a745daf61f70cc28aaa1705dea7197a26a
Konstantinos Stratis <stratis.konstantinos@gmail.com>
2022-05-24 01:18:21 +0300

    test 2 - skr settings

--- app/javascript/bundles/datatables/FilterDrawer.jsx
+++ app/javascript/bundles/datatables/FilterDrawer.jsx
@@ -33,29 +33,41 @@ function FilterDrawer(props) {
   // }, [filtersOpen, windowSize]);
 
   // useEffect(() => {
-  //   targetElement.current = document.querySelector('#filter-drawer');
+  //   targetElement.current = document.querySelector('#filter-drawer-element');
   // }, []);
 
   return (
+      // <div className={`filter-drawer-container ${windowSize[0] < 1200 && filtersOpen ? 'filter-drawer-container-active' : ''}`}>
+      <div className={`
+        ${
+          windowSize[0] < 1200
+              ? filtersOpen
+                  ? 'filter-drawer-container d-block d-xl-none filter-drawer-container-active'
+                  : 'd-none'
+              : filtersOpen
+                  ? 'filters col-xl-4 d-none d-lg-block animated fadeIn'
+                  : 'filters d-none animated fadeIn'
+      }`}>
     <div
-      id={'filter-drawer'}
+      id={'filter-drawer-element'}
       className={`
         ${
           windowSize[0] < 1200
             ? filtersOpen
-              ? 'side-drawer position-fixed d-block d-xl-none side-drawer-active'
-              : 'side-drawer position-fixed'
+              ? 'filter-drawer d-block d-xl-none filter-drawer-active'
+              : 'd-none'
             : filtersOpen
-            ? 'filters col-xl-4 d-none d-lg-block animated fadeIn'
+            ? 'filters animated fadeIn'
             : 'filters d-none animated fadeIn'
         }`}>
       <PropertyFilters {...props} />
-      <div className={`${windowSize[0] < 1200 ? '' : 'd-none'} drawer-controls py-3 px-5`}>
+      <div className={`${windowSize[0] < 1200 ? '' : 'd-none'} bottom-actions drawer-controls`}>
         <button className={'btn btn-primary btn-md btn-block'} onClick={e => props.handleChange()}>
           {props.i18n.ok}
         </button>
       </div>
     </div>
+      </div>
   );
 }
 
